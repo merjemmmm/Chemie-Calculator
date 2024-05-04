1c8a5922dfee0701886d57e9217e7b036ca30e2e
merjemmmm <162430321+merjemmmm@users.noreply.github.com>
2024-05-04 13:26:39 +0200

    Update Chemie_Calculator.py

--- Chemie_Calculator.py
+++ Chemie_Calculator.py
@@ -3,6 +3,7 @@ import streamlit as st
 # Funktion für die Startseite der App
 def homepage():
     st.title('Herzlich willkommen auf unserer App - Chemie Calculator')
+    st.image('path/to/beaker_image.png', width=100)
     st.subheader("🧪 Chemie Calculator")
     st.write('Diese App bietet Ihnen die Möglichkeit, häufig verwendete Aufgaben im Chemie Labor zu berechnen.')
     st.write('Wählen Sie eine Funktion aus und klicken Sie darauf.')
